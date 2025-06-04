85d0b02650118bf078ec12151eb1b19c40b1a4d1
neerajpython2025 <neerajguptagwl5@gmail.com>
2025-06-04 13:34:17 +0530

    Add files via upload

---
+++ assign1_task1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first Number: "))
+b=int(input("Enter the Second Number: "))
+
+c=a+b
+print("Addition:", c)
+c=a-b
+print("Subtraction:", c)
+c=a*b
+print("Multiplication:", c)
+c=a/b
+print("Division:", c)
---
+++ assign1_task2.py
@@ -0,0 +1,4 @@
+a=input("Enter the first Name: ")
+b=input("Enter the Second Name: ")
+c=a+b+"!"
+print("Hello,", c,"Welcome to the Python Program.")
